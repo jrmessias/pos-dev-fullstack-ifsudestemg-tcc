ef6914fb9f4da23365fe32ff4905a2cda8b82e66
Júnior Messias <jrmessias@gmail.com>
2026-01-30 13:35:32 -0300

    WIP

--- front/src/components/ProtectedRoute.js
+++ front/src/components/ProtectedRoute.js
@@ -1,13 +1,14 @@
-import { Navigate } from 'react-router-dom';
+import {Navigate, Outlet} from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
-export default function ProtectedRoute({ children, role }) {
+export default function ProtectedRoute({ role }) {
 
     const { user, loading } = useContext(AuthContext);
     if (loading) return null;
     if (!user) return <Navigate to="/login" replace />;
     if (role && user.role !== role) return <Navigate to="/unauthorized" replace />;
 
-    return children;
+    // return children;
+    return <Outlet />;
 }
--- front/src/routes.js
+++ front/src/routes.js
@@ -1,40 +1,49 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './contexts/AuthContext';
+import {AuthProvider} from './contexts/AuthContext';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Unauthorized from './pages/Unauthorized';
 import Dashboard from "./pages/dashboard/Dashboard.js";
 import NotFound from "./pages/NotFound.js";
+import LayoutSystem from "./layouts/LayoutSystem.js";
+import LayoutAuth from "./layouts/LayoutAuth.js";
 
 export default function AppRoutes() {
+
     return <>
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/unauthorized" element={<Unauthorized />} />
-                    <Route path="*" element={<NotFound />} />
+                    {/* Rota Pública - Home */}
+                    <Route path="/" element={<Home/>}/>
 
-                    <Route
-                        path="/teacher"
-                        element={
-                            <ProtectedRoute role="teacher">
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
+                    {/* Rota Pública - Erros */}
+                    <Route path="/unauthorized" element={<Unauthorized/>}/>
+                    <Route path="*" element={<NotFound/>}/>
 
-                    <Route
-                        path="/student"
-                        element={
-                            <ProtectedRoute role="student">
-                                <Dashboard />
-                            </ProtectedRoute>
-                        }
-                    />
+                    {/* Rota Pública - Login */}
+                    <Route element={<LayoutAuth />}>
+                        <Route path="/login" element={<Login />} />
+                    </Route>
+
+                    {/* Rota Privada - Sistema (protegida) */}
+                    {/* Área do Professor - Protegida */}
+                    <Route element={<ProtectedRoute role='teacher' />}>
+                        <Route path="/teacher" element={<LayoutSystem />}>
+                            <Route index element={<Dashboard />} />
+                            {/*<Route path="classes" element={<TeacherClasses />} />*/}
+                        </Route>
+                    </Route>
+
+                    {/* Área do Aluno - Protegida */}
+                    <Route element={<ProtectedRoute role='student' />}>
+                        <Route path="/student" element={<LayoutSystem />}>
+                            <Route index element={<Dashboard />} />
+                            {/*<Route path="courses" element={<StudentCourses />} />*/}
+                        </Route>
+                    </Route>
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
